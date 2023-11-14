d95b6c23915fd0cbaf3a21cb724fc9f2bc5ca3b2
DaneilBrisch <danielbrisch157@gmail.com>
2023-11-13 21:52:49 -0300

    aulaBosta

--- src/server.py
+++ src/server.py
@@ -1,8 +1,26 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-app = FastAPI()
+from src.config.database import create_db_and_tables, get_session
+from src.models.manutencao_model import Manutencao
 
-@app.get("/healthcheck")
-def healthcheck():
-    return {"status": "200 OK"}
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
+def root():
+    return {"message": "hello world"}
+
+@app.post("/manutencoes")
+def criar_manutencao(manutencao: Manutencao):
+    with get_session() as session:
+        session.add(manutencao)
+        session.commit()
+        session.refresh(manutencao)
+        return manutencao
